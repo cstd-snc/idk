c72955c2a555e5394cb871c5d15d2e3e4792e5f2
cstd-snc <xynklplug@gmail.com>
2026-09-14 15:32:33 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require ('express') ;
+const app = express() ;
+const port= 3000
+
+app.get('/', (req,res) => {
+    res.send('aplicação rodando em cointeiner docker!') ;
+});
+
+app.listen(port, () => {
+    console.log('servidor rodando na porta ${port}');
+});
