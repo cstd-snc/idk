d628fa39f329cfb98680020fbf97eefdeccae7a3
cstd-snc <xynklplug@gmail.com>
2026-09-16 16:02:38 -0300

    fix: adiciona env node e corrige template string

--- server.js
+++ server.js
@@ -8,4 +8,16 @@ app.get('/', (req,res) => {
 
 app.listen(port, () => {
     console.log('servidor rodando na porta ${port}');
+    /* eslint-env node */
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('aplicação rodando em container docker!');
+});
+
+app.listen(port, () => {
+    console.log(`servidor rodando na porta ${port}`);
+});
 }) ;
