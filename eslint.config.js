e17a1083d6a809e25fdb82ece9b6376e6cc1a24f
cstd-snc <xynklplug@gmail.com>
2026-09-16 16:11:40 -0300

    fix: adiciona flat config do eslint

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+﻿import globals from "globals"; import pluginJs from "@eslint/js"; export default [pluginJs.configs.recommended, { languageOptions: { globals: { ...globals.node, ...globals.commonjs } } }];
